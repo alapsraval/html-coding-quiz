26a44cdbd2a121de2e77bb0caa8a2937efd60b36
Alap Raval <alapsraval@gmail.com>
2021-04-28 21:24:26 -0500

    questions

---
+++ assets/questionBank.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Trainer Marking Language",
+            "Hyper Text Marketing Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Markup Leveler"
+        ]
+
+    },
+    {
+        question: "What is the correct sequence of HTML tags for starting a webpage?",
+        answer: "HTML, Head, Title",
+        options: [
+            "Head, Title, HTML",
+            "Title, Head, HTML",
+            "HTML, Head, Title"
+        ]
+
+    },
+    {
+        question: "How can you create an email link?",
+        answer: "A href='mailto:xxx@yyy'",
+        options: [
+            "A href='mailto:xxx@yyy'",
+            "A href='xxx@yyy'",
+            "Mail>xxx@yyy",
+            "Mail href='xxx@yyy'"
+        ]
+
+    }, {
+        question: "'radio' button used on a web page, would allow a person to select:",
+        answer: "Only one iteme",
+        options: [
+            "More than one item",
+            "Only one item"
+        ]
+
+    }
+]
