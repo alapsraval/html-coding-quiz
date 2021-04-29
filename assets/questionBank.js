1f6466787e3cd9a8a8918acb9cf80d4108a0d149
Alap Raval <alapsraval@gmail.com>
2021-04-28 21:37:55 -0500

    more questions

--- assets/questionBank.js
+++ assets/questionBank.js
@@ -32,11 +32,35 @@ var questions = [
 
     }, {
         question: "'radio' button used on a web page, would allow a person to select:",
-        answer: "Only one iteme",
+        answer: "Only one item",
         options: [
             "More than one item",
             "Only one item"
         ]
 
+    }, {
+        question: "How is document type initialized in HTML5?",
+        answer: "<!DOCTYPE HTML>",
+        options: [
+            "</DOCTYPE HTML>",
+            "</DOCTYPE>",
+            "<!DOCTYPE HTML>",
+            "</DOCTYPE html>"
+        ]
+
+    }, {
+        question: "How many heading tags are there in HTML5?",
+        answer: 6,
+        options: [2, 3, 5,6]
+    },
+    {
+        question: "Which of the following is the correct way to add background color in HTML?",
+        answer:"<body style = 'background-color: green;'>",
+        options: [
+            "<body color = 'green'>",
+            "<body style = 'background-color: green;'>",
+            "<body bg-color = 'green'>",
+            "<body style = 'background-color=green'>"
+        ]
     }
 ]
